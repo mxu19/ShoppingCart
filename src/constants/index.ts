a50a793edab1408f0baa41991b715b2307f8226e
mxu19 <mxu19>
2020-02-13 21:07:22 +0800

    add mathjs & update products

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,22 +1,27 @@
 export const products = [
   {
     name: "Plumpy Nut",
-    price: 25.76
+    price: 25.76,
+    total: 4690,
   },
   {
     name: "Measles Vaccine",
-    price: 90.51
+    price: 90.51,
+    total: 50066,
   },
   {
     name: "Therapeutic Milk",
-    price: 56.14
+    price: 56.14,
+    total: 9099,
   },
   {
     name: "Pencils",
-    price: 13.9
+    price: 13.9,
+    total: 4905,
   },
   {
     name: "School Bags",
-    price: 19.45
+    price: 19.45,
+    total: 9450,
   }
 ];
