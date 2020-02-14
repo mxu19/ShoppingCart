0fb4e4a17791bc8b2d0929a6d76af7144d5de243
mxu19 <mxu19>
2020-02-14 17:06:06 +0800

    add btn click Handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,252 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import * as math from "mathjs";
 
-import { products } from "./constants";
+import { Iproducts, products } from "./constants";
 
-const App = () => {
-  const proList = products.map((item, index) => {
-    return (
-      <div className="row tc pro-card" key={index}>
-        <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">{item.name}</div>
-        <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">${item.price}</div>
-        <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-          {item.totalNum}
-        </div>
-        <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-          <button type="button" className="btn btn-warning">
-            Add Car
-          </button>
-        </div>
-      </div>
-    );
-  });
+function printFn(value: number) {
+  const precision = 14;
+  return Number(math.format(value, precision));
+}
 
-  const buyList = products.map((item, index) => {
-    return (
-      <div className="row tc pro-card" key={index}>
-        <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">{item.name}</div>
-        <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">${item.price}</div>
-        <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-          {item.totalNum}
-        </div>
-        <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-          {item.totalNum}
-        </div>
-        <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-          <button type="button" className="btn btn-warning">
-            Add Car
-          </button>
-        </div>
-      </div>
-    );
-  });
+class App extends Component {
+  state = {
+    total: 0,
+    buyProList: [
+      // 132
+      {
+        id: 1,
+        name: "Plumpy Nut",
+        price: 25.76,
+        totalNum: 4690,
+        buynum: 2
+      }
+    ]
+  };
 
-  return (
-    <div className="App container-fluid">
-      <h1>Welcome to the mxu's shop</h1>
+  componentDidMount() {
+    let { buyProList, total } = this.state;
+    total = buyProList
+      .map(book => printFn(book.price * book.buynum))
+      .reduce((prev, cur) => prev + cur);
+    this.setState({
+      total
+    });
+  }
 
-      <div className="row">
-        <div className="col-xs-5 col-sm-5 col-md-5 col-lg-5">
-          <h2>Products List</h2>
+  setproList = (it: Iproducts, type: string) => {
+    let { buyProList, total } = this.state;
+    let exist = false;
+    let isexist = false;
 
-          <div className="list-wrap">
-            <div className=" title-top tc">
-              <div className="row">
-                <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                  Product Name
-                </div>
-                <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                  Product Price
-                </div>
-                <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                  Products Quantity
-                </div>
-                <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                  Action
-                </div>
-              </div>
-            </div>
-            <div className="pro-wrap ">{proList}</div>
+    if (type === "add") {
+      if (buyProList.length) {
+        buyProList.forEach(item => {
+          if (item.id === it.id) {
+            item.buynum += 1;
+            exist = true;
+
+            products.forEach(item => {
+              if (item.id === it.id) {
+                item.totalNum -= 1;
+              }
+            });
+          }
+        });
+        this.setState({
+          buyProList
+        });
+      }
+
+      if (!exist) {
+        buyProList = buyProList.concat(Object.assign({}, it, { buynum: 1 }));
+        this.setState({
+          buyProList
+        });
+        products.forEach(item => {
+          if (item.id === it.id) {
+            item.totalNum = item.totalNum - 1;
+          }
+        });
+      }
+    }
+
+    if (type === "edd") {
+      if (buyProList.length) {
+        buyProList.forEach(item => {
+          if (item.id === it.id && item.buynum > 1) {
+            item.buynum -= 1;
+          }
+        });
+        this.setState({
+          buyProList
+        });
+      }
+    }
+
+    total = buyProList
+      .map(book => printFn(book.price * book.buynum))
+      .reduce((prev, cur) => prev + cur, 0);
+    this.setState({
+      total
+    });
+  };
+
+  delItem = (it: Iproducts) => {
+    let { buyProList, total } = this.state;
+    let newlist = buyProList;
+
+    let index = buyProList.findIndex(item => item.id === it.id);
+    newlist.splice(index, 1);
+
+    this.setState({
+      buyProList: newlist
+    });
+
+    total = buyProList
+      .map(book => printFn(book.price * book.buynum))
+      .reduce((prev, cur) => prev + cur, 0);
+    this.setState({
+      total
+    });
+  };
+
+  render() {
+    const proList = products.map((item, index) => {
+      return (
+        <div className="row tc pro-card" key={index}>
+          <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">{item.name}</div>
+          <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+            ${item.price}
+          </div>
+          <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+            {item.totalNum}
+          </div>
+          <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+            <button
+              type="button"
+              onClick={() => this.setproList(item, "add")}
+              className="btn btn-warning"
+            >
+              Add Car
+            </button>
           </div>
         </div>
-        <div className="col-xs-7 col-sm-7 col-md-7 col-lg-7">
-          <h2>Shopping Cart</h2>
-          <div className="list-wrap">
-            <div className=" title-top tc">
-              <div className="row">
-                <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-                  Product Name
-                </div>
-                <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-                  Product Price
-                </div>
-                <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-                  Purchase Quantity
-                </div>
-                <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-                  Amount of Money
-                </div>
-                <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                  Action
-                </div>
-              </div>
-            </div>
-            <div className="pro-wrap ">{buyList}</div>
+      );
+    });
+
+    const buyList = this.state.buyProList.map((item, index) => {
+      return (
+        <div className="row tc pro-card" key={index}>
+          <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">{item.name}</div>
+          <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+            ${item.price}
+          </div>
+          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+            <button
+              type="button"
+              onClick={() => this.setproList(item, "edd")}
+              className="btn btn-default btn-sm btn-num"
+            >
+              -
+            </button>
+            <span>{item.buynum}</span>
+            <button
+              type="button"
+              onClick={() => this.setproList(item, "add")}
+              className="btn btn-default btn-sm btn-num"
+            >
+              +
+            </button>
+          </div>
+          <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+            ${printFn(item.buynum * item.price)}
+          </div>
+          <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+            <button
+              type="button"
+              onClick={() => this.delItem(item)}
+              className="btn btn-danger"
+            >
+              Delete Item
+            </button>
           </div>
         </div>
+      );
+    });
+
+    return (
+      <div className="App container-fluid">
+        <h1>Welcome to the mxu's shop</h1>
+
+        <div className="row">
+          <div className="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+            <h2>Products List</h2>
+
+            <div className="list-wrap">
+              <div className=" title-top tc">
+                <div className="row">
+                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    Product Name
+                  </div>
+                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    Product Price
+                  </div>
+                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    Products Quantity
+                  </div>
+                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    Action
+                  </div>
+                </div>
+              </div>
+              <div className="pro-wrap ">{proList}</div>
+            </div>
+          </div>
+          <div className="col-xs-7 col-sm-7 col-md-7 col-lg-7">
+            <h2>Shopping Cart</h2>
+            <div className="list-wrap">
+              <div className=" title-top tc">
+                <div className="row">
+                  <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                    Product Name
+                  </div>
+                  <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                    Product Price
+                  </div>
+                  <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    Purchase Quantity
+                  </div>
+                  <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                    Amount of Money
+                  </div>
+                  <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                    Action
+                  </div>
+                </div>
+              </div>
+              <div className="pro-wrap ">{buyList}</div>
+            </div>
+            <div className="row">
+              <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                total {this.state.buyProList.length} items selected
+              </div>
+              <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                Total: ${printFn(this.state.total)}
+              </div>
+              <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4"></div>
+              <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div>
+              <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
-      <br />
-      <br />
-      <br />
-      <br />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,45 +1,63 @@
-export const products = [
+export interface Iproducts {
+  name: string;
+  id: number;
+  price: number;
+  totalNum: number;
+  buynum?: number;
+}
+
+
+export const products: Iproducts[] = [
   {
+    id: 1,
     name: "Plumpy Nut",
     price: 25.76,
     totalNum: 4690,
   },
   {
+    id: 2,
     name: "Measles Vaccine",
     price: 90.51,
     totalNum: 50066,
   },
   {
+    id: 3,
     name: "Therapeutic Milk",
     price: 56.14,
     totalNum: 9099,
   },
   {
+    id: 4,
     name: "Pencils",
     price: 13.9,
     totalNum: 4905,
   },
   {
+    id: 5,
     name: "Pencils",
     price: 13.9,
     totalNum: 4905,
   },
   {
+    id: 6,
     name: "Pencils",
     price: 13.9,
     totalNum: 4905,
   },
   {
+    id: 7,
     name: "Pencils",
     price: 13.9,
     totalNum: 4905,
   },
   {
+    id: 8,
     name: "Pencils",
     price: 13.9,
     totalNum: 4905,
   },
   {
+    id: 9,
     name: "School Bags",
     price: 19.45,
     totalNum: 9450,
